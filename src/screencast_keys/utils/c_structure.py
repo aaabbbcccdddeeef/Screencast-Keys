1fb5fe108aa941898507678b7bbf28c76ef0ad7c
nutti <nutti.metro@gmail.com>
2023-02-24 18:05:55 +0900

    WIP

--- src/screencast_keys/utils/c_structure.py
+++ src/screencast_keys/utils/c_structure.py
@@ -1,9 +1,11 @@
+import argparse
 import requests
 import re
+import sys
 
 SOURCE_BASE_URL = "https://raw.githubusercontent.com/blender/blender/"
 
-def gen_import():
+def gen_import(file):
     body = '''from ctypes import (
     c_void_p, c_char, c_short, c_int, c_int8,
     addressof, cast, pointer,
@@ -12,7 +14,7 @@ def gen_import():
 )
     '''
 
-    print(f"{body}\n")
+    print(f"{body}\n", file=file)
 
 
 def parse_struct(code_body, struct_name):
@@ -101,38 +103,7 @@ def type_to_ctype(type, is_pointer):
     return False, "c_void_p"
 
 
-def gen_enum(tag, source_file_path, enum_name, add_method_func):
-    url = f"{SOURCE_BASE_URL}/{tag}/{source_file_path}"
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    code_body = response.text
-    lines = code_body.split("\n")
-    in_enum = False
-    enum_code_body = ""
-    for l in lines:
-        if not in_enum:
-            m = re.search(r"enum\s+" + enum_name + r"\s+{$", l)
-            if m:
-                in_enum = True
-        else:
-            enum_code_body += l + "\n"
-            m = re.search("^}", l)
-            if m:
-                break
-    else:
-        raise Exception(f"Enumerator {enum_name} is not found.")
-
-    items = parse_enum(enum_code_body, enum_name)
-
-    print("# pylint: disable=C0103")
-    print(f"class {enum_name}:")
-    print(f'    """Defined in ${source_file_path}"""\n')
-    for item in items:
-        print(f'    {item["name"]} = {item["value"]}')
-    print("\n")
-
-def gen_struct(tag, source_file_path, struct_name, add_method_func):
+def gen_struct(file, tag, source_file_path, struct_name, add_method_func, add_variable_func):
     url = f"{SOURCE_BASE_URL}/{tag}/{source_file_path}"
     response = requests.get(url)
     response.raise_for_status()
@@ -156,23 +127,62 @@ def gen_struct(tag, source_file_path, struct_name, add_method_func):
 
     variables = parse_struct(struct_code_body, struct_name)
 
-    print(f"class {struct_name}(Structure):")
-    print(f'    """Defined in ${source_file_path}"""')
-    if callable(add_method_func):
-        print(add_method_func())
-    print("\n")
-    print("# pylint: disable=W0212")
-    print(f"{struct_name}._field_ = [")
-    for var in variables:
+    def print_variable(file, var):
         known_type, type_name = type_to_ctype(var["type"], var["is_pointer"])
         if var["array_element_num"]:
             type_name = f'{type_name} * {var["array_element_num"]}'
         if known_type:
-            print(f'    ("{var["name"]}", {type_name}),')
+            print(f'    ("{var["name"]}", {type_name}),', file=file)
         else:
-            print(f'    ("{var["name"]}", {type_name}),   # {var["type"]}')
-    print("]")
-    print("\n")
+            print(f'    ("{var["name"]}", {type_name}),   # {var["type"]}', file=file)
+
+    print(f"class {struct_name}(Structure):", file=file)
+    print(f'    """Defined in ${source_file_path}"""', file=file)
+    if callable(add_method_func):
+        print(add_method_func(), file=file)
+    print("\n", file=file)
+    print("# pylint: disable=W0212", file=file)
+    print(f"{struct_name}._fields_ = [", file=file)
+    for var in variables:
+        print_variable(file, var)
+    if callable(add_variable_func):
+        variables_to_add = add_variable_func()
+        for var in variables_to_add:
+            print_variable(file, var)
+    print("]", file=file)
+    print("\n", file=file)
+
+
+def gen_enum(file, tag, source_file_path, enum_name):
+    url = f"{SOURCE_BASE_URL}/{tag}/{source_file_path}"
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    code_body = response.text
+    lines = code_body.split("\n")
+    in_enum = False
+    enum_code_body = ""
+    for l in lines:
+        if not in_enum:
+            m = re.search(r"enum\s+" + enum_name + r"\s+{$", l)
+            if m:
+                in_enum = True
+        else:
+            enum_code_body += l + "\n"
+            m = re.search("^}", l)
+            if m:
+                break
+    else:
+        raise Exception(f"Enumerator {enum_name} is not found.")
+
+    items = parse_enum(enum_code_body, enum_name)
+
+    print("# pylint: disable=C0103", file=file)
+    print(f"class {enum_name}:", file=file)
+    print(f'    """Defined in ${source_file_path}"""\n', file=file)
+    for item in items:
+        print(f'    {item["name"]} = {item["value"]}', file=file)
+    print("\n", file=file)
 
 
 def add_method_for_ListBase():
@@ -243,24 +253,50 @@ def add_method_for_ListBase():
     return body
 
 
-def main():
-    tag = "v3.0.0"
-    gen_info = [
-        ["enum", "source/blender/windowmanager/wm_event_system.h", "eWM_EventHandlerType", None],
-        ["struct", "source/blender/makesdna/DNA_listBase.h", "Link", None],
-        ["struct", "source/blender/makesdna/DNA_listBase.h", "ListBase", add_method_for_ListBase],
-        ["struct", "source/blender/makesdna/DNA_screen_types.h", "ScrAreaMap", None],
-        ["struct", "source/blender/makesdna/DNA_windowmanager_types.h", "wmWindow", None],
-        ["struct", "source/blender/makesdna/DNA_windowmanager_types.h", "wmOperator", None],
-        ["struct", "source/blender/windowmanager/wm_event_system.h", "wmEventHandler", None],
+def add_variable_for_wmEventHandler():
+    variables = [
+        {
+            "name": "op",
+            "type": "wmOperator",
+            "is_pointer": True,
+            "array_element_num": None,
+        },
     ]
 
-    gen_import()
+    return variables
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", nargs="?", type=argparse.FileType("w"), default=sys.stdout)
+    parser.add_argument("-t", "--target", nargs="?", type=str, default="main")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_argument()
+
+    output_file = args.output
+    target = args.target
+    gen_info = [
+        ["enum", "source/blender/windowmanager/wm_event_system.h", "eWM_EventHandlerType"],
+
+        ["struct", "source/blender/makesdna/DNA_listBase.h", "Link", None, None],
+        ["struct", "source/blender/makesdna/DNA_listBase.h", "ListBase", add_method_for_ListBase, None],
+        ["struct", "source/blender/makesdna/DNA_screen_types.h", "ScrAreaMap", None, None],
+        ["struct", "source/blender/makesdna/DNA_windowmanager_types.h", "wmWindow", None, None],
+        ["struct", "source/blender/makesdna/DNA_windowmanager_types.h", "wmOperator", None, None],
+        ["struct", "source/blender/windowmanager/wm_event_system.h", "wmEventHandler", None, add_variable_for_wmEventHandler],
+    ]
+
+    gen_import(output_file)
     for info in gen_info:
         if info[0] == "struct":
-            gen_struct(tag, info[1], info[2], info[3])
+            gen_struct(output_file, target, info[1], info[2], info[3], info[4])
         elif info[0] == "enum":
-            gen_enum(tag, info[1], info[2], info[3])
+            gen_enum(output_file, target, info[1], info[2])
 
-
-main()
+if __name__ == "__main__":
+    main()
